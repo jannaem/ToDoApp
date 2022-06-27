322de5ce1076ea44bd96d5d4bf46ef479b40f82f
Milena-Blaser <blaser@noseryoung.com>
2022-06-27 21:27:34 +0200

    added tests

--- todoapp/src/main/java/tbz/project/todoapp/toDoList/ToDoListDTO.java
+++ todoapp/src/main/java/tbz/project/todoapp/toDoList/ToDoListDTO.java
@@ -3,13 +3,14 @@ package tbz.project.todoapp.toDoList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import tbz.project.todoapp.task.Task;
 
 @AllArgsConstructor
 @Getter
 @Setter
-
+@NoArgsConstructor
 public class ToDoListDTO {
   private String name;
   private List<Task> tasks;
